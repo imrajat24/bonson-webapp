db35c4a13374079083ace057463de43377388e18
sid-talesara <2020pcecssiddharth207@poornima.org>
2022-12-19 17:54:59 +0530

    commit 16 info section responsive

--- components/about/Info.jsx
+++ components/about/Info.jsx
@@ -24,6 +24,7 @@ const Info = () => {
           src={mainImg}
           alt="warehouse image"
           className="info--img"
+          layout="fill"
           objectFit="cover"
         />
       </div>
