6fdf5399679ae75e7562220538b2dbb4f4811f99
sid-talesara <2020pcecssiddharth207@poornima.org>
2022-12-19 19:58:25 +0530

    commit 21 our history section responsive

--- components/about/OurHistory.jsx
+++ components/about/OurHistory.jsx
@@ -1,28 +1,51 @@
 import React from "react";
 import Image from "next/image";
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 // images
-import team1 from "../../assets/images/team-img-1.jpg";
+import team1 from "../../assets/images/history-slider.png";
 import Link from "next/link";
 
 const OurHistory = () => {
+  // ! Object for carosuel responsiveness
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <div className="history__wrap">
       <div className="history row">
-        <h2 className="history--heading">Our History</h2>
+        <div className="section__heading">
+          <h2 className="history--heading">Our History</h2>
+        </div>
 
-        <div className="history__cardWrap">
+        <Carousel responsive={responsive} showDots={true} focusOnSelect={true}>
+          {/* card starts */}
           <div className="history__card">
             <div className="history__graphics">
               <div className="history__graphics--image img1">
-                <Image src={team1} alt="team" objectFit="cover" layout="fill" />
-              </div>
-              <div className="history__graphics--image img2">
-                <Image src={team1} alt="team" objectFit="cover" layout="fill" />
-              </div>
-
-              <div className="history__graphics--image img3">
-                <Image src={team1} alt="team" objectFit="cover" layout="fill" />
+                <Image
+                  src={team1}
+                  alt="team"
+                  layout="fill"
+                  objectFit="contain"
+                />
               </div>
             </div>
             <div className="history__content">
@@ -33,7 +56,70 @@ const OurHistory = () => {
               </p>
             </div>
           </div>
-        </div>
+
+          {/* card starts */}
+          <div className="history__card">
+            <div className="history__graphics">
+              <div className="history__graphics--image img1">
+                <Image
+                  src={team1}
+                  alt="team"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            </div>
+            <div className="history__content">
+              <h3 className="history__content--year">1998</h3>
+              <p className="history__content--para">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+                sed corporis nam ad dolore dicta id nisi fuga, adipisci quod!
+              </p>
+            </div>
+          </div>
+
+          {/* card starts */}
+          <div className="history__card">
+            <div className="history__graphics">
+              <div className="history__graphics--image img1">
+                <Image
+                  src={team1}
+                  alt="team"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            </div>
+            <div className="history__content">
+              <h3 className="history__content--year">1998</h3>
+              <p className="history__content--para">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+                sed corporis nam ad dolore dicta id nisi fuga, adipisci quod!
+              </p>
+            </div>
+          </div>
+
+          {/* card starts */}
+          <div className="history__card">
+            <div className="history__graphics">
+              <div className="history__graphics--image img1">
+                <Image
+                  src={team1}
+                  alt="team"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            </div>
+            <div className="history__content">
+              <h3 className="history__content--year">1998</h3>
+              <p className="history__content--para">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+                sed corporis nam ad dolore dicta id nisi fuga, adipisci quod!
+              </p>
+            </div>
+          </div>
+        </Carousel>
       </div>
     </div>
   );
