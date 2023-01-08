b1d7ae2bfc11c4eddb4ec95cf72c23608d06e2c7
sid-talesara <2020pcecssiddharth207@poornima.org>
2023-01-08 21:45:29 +0530

    bug removed

--- components/Product/ProductSlider.jsx
+++ components/Product/ProductSlider.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Image from "next/image";
 import { useState } from "react";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/thumbs";
-// import { Navigation, Thumbs } from "swiper";
+import { Navigation, Thumbs } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 // images
 import mainImg from "../../assets/images/66kv product img.png";
 import mainImg2 from "../../assets/images/66kv product img - Copy.png";
